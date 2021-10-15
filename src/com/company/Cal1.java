7585b5106af2cbbaf323ea802687b2b39aafd850
OHpetro <petiamatiu9@gmail.com>
2021-10-15 20:32:01 +0300

    method 1an2.1

--- src/com/company/Cal1.java
+++ src/com/company/Cal1.java
@@ -1,16 +1,16 @@
 package com.company;
 
-//метод 3  (калькулятор для строки по типу \\д*,\\д* для любого колическва чисел и с любым разделителем)
+//метод 1 и 2  (калькулятор для строки по типу \\д*,\\д* для любого колическва чисел и с любым разделителем)
 
 public class Cal1 {
     public static void Add(String str){
 
         int res = 0;
 
-        if(str.matches("^([0-9]*+\\D*+[0-9]*)*$") == false){
+        if(str.matches("^([0-9]*+,*)*$") == false){   //упростил
             System.out.println("Ваш ввод был неправельным ");
         }else{
-            String[] value = str.split("\\D+");
+            String[] value = str.split(",");
             for(int i=0;i<value.length;i++){
                 int strtoint = 0;
                 try{
